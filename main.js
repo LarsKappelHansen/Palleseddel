82af0c3ab1cebf718a8087d888e54eab6df89e67
Lars Kappel Hansen <91117491+LarsKappelHansen@users.noreply.github.com>
2022-02-16 20:03:56 +0100

    Version 1

---
+++ main.js
@@ -0,0 +1,224 @@
+﻿// JavaScript source code
+function onresize() {
+    refresh();
+}
+
+var lastCanvasWidth = 0;
+
+function refresh() {
+    //var viewWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    //var viewWidth = window.innerWidth;
+    var viewWidth = document.documentElement.clientWidth;
+    if (viewWidth > 800) {
+        viewWidth = 800;
+    }
+
+    var viewPaddings = 60;
+    var elementLeftMargin = 0;
+    setWidth('section', (viewWidth - viewPaddings));
+    setWidth('elementNumberField', (viewWidth - viewPaddings - elementLeftMargin - 14));
+    setWidth('elementTextField', (viewWidth - viewPaddings - elementLeftMargin - 14));
+    setWidth('dropbtn', (viewWidth - viewPaddings));
+    setWidth('dropelement', (viewWidth - viewPaddings - 2));
+    var canvasWidth = viewWidth - viewPaddings - elementLeftMargin;
+    if (canvasWidth != lastCanvasWidth) {
+        setCanvasWidth(canvasWidth);
+        lastCanvasWidth = canvasWidth;
+    }
+    SetSendButtonPosition(viewWidth - viewPaddings);
+
+}
+
+function setWidth(className, width) {
+    const elements = document.getElementsByClassName(className);
+    for (element of elements) {
+        element.style.width = width + 'px';
+    }
+}
+
+function setCanvasWidth(width) {
+    canvas.width = 2 * width;
+    canvas.style = 'width: ' + width + 'px; height: 200px';
+}
+
+function SetSendButtonPosition(width) {
+    var divSend = document.getElementById("divSend");
+    divSend.style.width = width + 'px';
+}
+
+// create canvas element and append it to document body
+var canvas = document.getElementById("canvas");
+
+// get canvas 2D context and set him correct size
+var canvasContext = canvas.getContext('2d');
+
+const canvasState = {
+    mousedown: false
+};
+
+// last known position
+var canvasPos = { x: 0, y: 0 };
+
+
+canvas.addEventListener('mousedown', handleWritingStart);
+canvas.addEventListener('mousemove', handleWritingInProgress);
+canvas.addEventListener('mouseup', handleDrawingEnd);
+canvas.addEventListener('mouseout', handleDrawingEnd);
+
+canvas.addEventListener('touchstart', handleWritingStart);
+canvas.addEventListener('touchmove', handleWritingInProgress);
+canvas.addEventListener('touchend', handleDrawingEnd);
+
+function handleWritingStart(e) {
+    if (!canvasFullyVisible()) {
+        return;
+    }
+
+    e.preventDefault();
+
+    document.activeElement.blur();
+
+    setPosition(e);
+
+    canvasContext.beginPath(); // begin
+
+    canvasContext.moveTo(canvasPos.x, canvasPos.y);
+
+    canvasContext.lineWidth = 3;
+    canvasContext.lineCap = 'round';
+    canvasContext.strokeStyle = 'black';
+
+    canvasState.mousedown = true;
+}
+
+function canvasFullyVisible() {
+    return (window.scrollY + window.innerHeight) >= (canvas.offsetTop + canvas.offsetHeight);
+    //console.log("window.scrollY " + window.scrollY);
+    //console.log("window.innerHeight " + window.innerHeight);
+    //console.log("canvas.offsetHeight " + canvas.offsetHeight);
+    //console.log("window.scrollY + window.innerHeight " + (window.scrollY + window.innerHeight));
+    //console.log("canvas.offsetTop  + canvas.offsetHeight " + );
+}
+
+function handleWritingInProgress(e) {
+    e.preventDefault();
+
+    if (canvasState.mousedown) {
+        setPosition(e);
+        canvasContext.lineTo(canvasPos.x, canvasPos.y); // to
+
+        canvasContext.stroke(); // draw it!
+    }
+}
+
+function handleDrawingEnd(e) {
+    e.preventDefault();
+
+    canvasState.mousedown = false;
+
+
+}
+
+// new position from mouse event
+function setPosition(e) {
+
+    var rect = canvas.getBoundingClientRect(), // abs. size of element
+        scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+        scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+    var clientX = e.clientX || e.touches[0].clientX;
+    var clientY = e.clientY || e.touches[0].clientY;
+
+    canvasPos.x = (clientX - rect.left) * scaleX;
+    canvasPos.y = (clientY - rect.top) * scaleY;
+}
+
+function clearCanvas() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function clearPaller() {
+    clearPalleField("numEurpalle11");
+    clearPalleField("numEurpalle12");
+    clearPalleField("numEurpalle14");
+}
+
+function clearPalleField(fieldName) {
+    var palleField = document.getElementById(fieldName);
+    palleField.value = '';
+}
+
+function clearAndet() {
+    var andetField = document.getElementById("buttonAndet");
+    andetField.innerHTML = 'Vælg fra liste...';
+}
+
+function clearKommentar() {
+    var palleField = document.getElementById("textFieldKommentar");
+    palleField.value = '';
+}
+
+
+
+function onSend() {
+    //Send...
+    clearForm();
+    refresh();
+}
+
+function clearForm() {
+    clearPaller();
+    clearAndet();
+    clearKommentar();
+    clearCanvas()
+}
+
+function buttonAndetClick() {
+    document.getElementById("andetDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function onSelectAndet(selection) {
+    document.getElementById("andetDropdown").classList.toggle("show");
+    var andetField = document.getElementById("buttonAndet");
+    andetField.innerHTML = selection;
+}
+
+
+//*** PWA stuff
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    //event.prompt();
+    // wait for the response from the deferred prompt
+    console.log('beforeinstallprompt');
+    event.userChoice.then((res) => {
+        // did the user approve installation?
+        if (res.outcome === 'accepted') {
+            console.log('doInstall: accepted');
+        } else {
+            console.log('doInstall: declined');
+        }
+    });
+});
+
+// register an event listener for after the app installs
+window.addEventListener('appinstalled', (event) => {
+    console.log('App Installed');
+});
+
+
+refresh();
+
+
---
+++ sw.js
@@ -0,0 +1,95 @@
+const cacheName = 'palleseddel';
+const appShellFiles = [
+    './',
+    './app.json',
+    './favicon.ico',
+    './favicon-16x16.png',
+    './favicon-32x32.png',
+    './favicon-96x96.png',
+    './index.html',
+    './main.css',
+    './main.html',
+    './main.js',
+    './sw.js',
+    './icons/android-icon-36x36.png',
+    './icons/android-icon-48x48.png',
+    './icons/android-icon-72x72.png',
+    './icons/android-icon-96x96.png',
+    './icons/android-icon-144x144.png',
+    './icons/android-icon-192x192.png',
+    './icons/android-icon-512x512.png',
+    './icons/apple-icon.png',
+    './icons/apple-icon-57x57.png',
+    './icons/apple-icon-60x60.png',
+    './icons/apple-icon-72x72.png',
+    './icons/apple-icon-76x76.png',
+    './icons/apple-icon-114x114.png',
+    './icons/apple-icon-120x120.png',
+    './icons/apple-icon-144x144.png',
+    './icons/apple-icon-152x152.png',
+    './icons/apple-icon-180x180.png',
+    './icons/apple-icon-precomposed.png',
+    './icons/favicon.ico',
+    './icons/favicon-16x16.png',
+    './icons/favicon-32x32.png',
+    './icons/favicon-96x96.png',
+    './icons/ms-icon-70x70.png',
+    './icons/ms-icon-144x144.png',
+    './icons/ms-icon-150x150.png',
+    './icons/ms-icon-310x310.png'
+];
+
+async function cacheFiles() {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(appShellFiles);
+    console.log('files cached');
+}
+
+self.addEventListener('install', event => {
+    // fires when the browser installs the app
+    // here we're just logging the event and the contents
+    // of the object passed to the event. the purpose of this event
+    // is to give the service worker a place to setup the local 
+    // environment after the installation completes.
+    console.log(`Event fired: ${event.type}`);
+    console.dir(event);
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(appShellFiles);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    // fires after the service worker completes its installation. 
+    // It's a place for the service worker to clean up from previous 
+    // service worker versions
+    console.log(`Event fired: ${event.type}`);
+    console.dir(event);
+});
+
+self.addEventListener('fetch', event => {
+    // Fires whenever the app requests a resource (file or data)
+    // normally this is where the service worker would check to see
+    // if the requested resource is in the local cache before going
+    // to the server to get it. There's a whole chapter in the book
+    // covering different cache strategies, so I'm not going to say 
+    // any more about this here
+    console.log(`Fetching ${event.request.url}`);
+    // console.dir(event.request);
+    // Next, go get the requested resource from the network, 
+    // nothing fancy going on here.
+    event.respondWith((async () => {
+        const r = await caches.match(event.request);
+        console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(event.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+        cache.put(event.request, response.clone());
+        return response;
+    })());
+});
